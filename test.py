b838e3d30d4d2e211ee49343e56e79c3909e2d9f
Eric Knickmeyer <ericknickmeyer@Students-MacBook-Pro-2.local>
2018-12-22 17:33:46 -0500

    adding this file to my github repo

---
+++ test.py
@@ -0,0 +1,10 @@
+# eric knickmeyer 
+# test python file
+
+import webbrowser
+
+print ("this is a test. Hello World.")
+print( 'anotha ones')
+
+
+webbrowser.open('https://www.google.com') #automation test!!!!
